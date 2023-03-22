50f59a1ec4cb5ba16c8f3643d0353eddd28e7026
Maks Thimbarov <110392935+vagabbond@users.noreply.github.com>
2023-03-22 16:08:56 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from "expo-status-bar";
+
+import {
+  StyleSheet,
+  ImageBackground,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import React, { useState } from "react";
+import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen/LoginScreen";
+
+const backImage = require("./Source/Photo_BG.png");
+
+export default function App() {
+  const [activeScreen, setActiveScreen] = useState(0);
+  const changeScrennFunc = (value) => {
+    setActiveScreen(value);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.maincontainer}>
+        <ImageBackground source={backImage} style={styles.backImg}>
+          {activeScreen === 0 ? (
+            <LoginScreen changeScrenn={changeScrennFunc} />
+          ) : (
+            <RegistrationScreen changeScrenn={changeScrennFunc} />
+          )}
+        </ImageBackground>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  maincontainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  backImg: {
+    flex: 1,
+    justifyContent: "flex-end",
+    width: "100%",
+  },
+});
